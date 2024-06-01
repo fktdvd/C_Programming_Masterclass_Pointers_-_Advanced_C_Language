b277c9734f03634e8002dce6d028aa90af77cd84
fktdvd <118221284+fktdvd@users.noreply.github.com>
2024-06-01 23:18:12 +0200

    commit

---
+++ 06_Printing_a_value_and_address/main.c
@@ -0,0 +1,18 @@
+/**
+ * Példa a Lecture 9-hez.
+ * 2024-06-01
+ * fktdvd
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int grade1 = 80, grade2 = 100;
+    printf("Grade1 : %d\n",grade1);
+    printf("Grade1 : %p\n",&grade1);
+
+    printf("Grade2 : %d\n",grade2);
+    printf("Grade2 : %p\n",&grade2);
+    return 0;
+}
