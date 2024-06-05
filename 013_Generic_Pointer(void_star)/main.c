e8b9b651f28cca734457ee1b88d0b9262115ce8d
fktdvd <118221284+fktdvd@users.noreply.github.com>
2024-06-05 13:41:09 +0000

    update

--- 013_Generic_Pointer(void_star)/main.c
+++ 013_Generic_Pointer(void_star)/main.c
@@ -1,18 +1,37 @@
 /**
- * Példa a Lecture 26-hez.
+ * Példa a Lecture 26-27-hez.
  * void * points to an address of ANY TYPE of data
- * 2024-06-03
+ * 2024-06-05
  * fktdvd
  */
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+void genericSwap(void *a, void *b, int size) // we assume that size > 0
+{
+    // size - specifies the number of bytes
+    void *tempMemory = malloc(size);
+
+    // Memory copy Function
+    // void * memcpy(void *dest, const void *src, size_t num)
+    memcpy(tempMemory, a, size);
+    memcpy(a, b, size);
+    memcpy(b, tempMemory, size);
+}
 
 int main()
 {
-    int a = 10;
-    void *ptr = &a;
-    printf("%d\n", *(int*)ptr);   //explicit typecasting, meg kell adni, hogy hány byte memóriát akarunk kiolvasni
+    // int a = 10;
+    // void *ptr = &a;
+    // printf("%d\n", *(int *)ptr); // explicit typecasting, meg kell adni, hogy hány byte memóriát akarunk kiolvasni
+
+    int num1 = 5, num2 = 7;
+    genericSwap(&num1, &num2, sizeof(int));
+    double dnum1 = 55.55, dnum2 = 66.66;
+    genericSwap(&dnum1, &dnum2, sizeof(double));
 
     return 0;
-
 }
