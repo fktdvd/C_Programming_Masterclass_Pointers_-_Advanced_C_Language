ce8521f7f6551c368a0a1c53e410e84a0d76eed7
fktdvd <118221284+fktdvd@users.noreply.github.com>
2024-06-08 14:28:37 +0200

    0

---
+++ 015_Memory_representation/main.c
@@ -0,0 +1,14 @@
+/**
+ * Példa a Lecture 28-hoz
+ * 2024-06-06
+ * fktdvd
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    long grades[3] = {3333333,5555555,7777777};
+    long *ptr = grades;
+    return 0;
+}
