04ba4ce4e4e45ae2adeb5a493bb08b357e7aa409
fktdvd <118221284+fktdvd@users.noreply.github.com>
2024-06-10 09:30:50 +0000

    0

--- 020_/main.c
+++ 020_/main.c
@@ -6,8 +6,31 @@
 
 #include <stdio.h>
 
+#define SIZE 4
+
+// InputArrey Function
+void InputArrey(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("Enter value #%d:", i + 1);
+        scanf("%d", &arr[i]);
+    }
+}
+
+// DisplayArrey Function
+void DisplayArrey(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("Value #%d: %d\n", i + 1, arr[i]);
+    }
+}
+
 int main()
 {
-    
+    int grades[SIZE];
+    InputArrey(grades, SIZE);
+    DisplayArrey(grades,SIZE);
     return 0;
 }
